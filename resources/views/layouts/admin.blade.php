41b5a7cc59e7b801c2c5ba9b139de2db3e4be57b
Mikael Jorhult <mikael@jorhult.se>
2017-09-22 20:02:37 +0200

    Base views for layout.

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Departur') }}</title>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <h1>
+            <a class="logo" href="{{ url('/') }}">
+                {{ config('app.name', 'Departur') }}
+            </a>
+        </h1>
+
+        <nav class="main-navigation">
+            <ul>
+                <li><a href="{{ route('schedules.index') }}">Schedules</a></li>
+                <li><a href="{{ route('calendars.index') }}">Calendars</a></li>
+                <li><a href="{{ route('users.index') }}">Users</a></li>
+                <li>
+                    <a href="{{ route('logout') }}"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                          style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="main-content">
+        <header>
+
+        </header>
+
+        @yield('content')
+
+        <footer>
+
+        </footer>
+    </main>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,20 +5,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Departur') }}</title>
 
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
     <h1>
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+            {{ config('app.name', 'Departur') }}
         </a>
     </h1>
 
