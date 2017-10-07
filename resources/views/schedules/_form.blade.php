160ba405d15919678b08b23940caea92937f35a8
Mikael Jorhult <mikael@jorhult.se>
2017-10-07 19:34:41 +0200

    Show attached calendars in schedule form.

--- resources/views/schedules/_form.blade.php
+++ resources/views/schedules/_form.blade.php
@@ -1,13 +1,33 @@
-<div class="form-group">
-    {!! Form::label('name', 'Name') !!}
-    {!! Form::text('name', null, ['placeholder' => 'Name']) !!}
-</div>
+<section class="row">
+    <div class="column">
+        <div class="form-group">
+            {!! Form::label('name', 'Name') !!}
+            {!! Form::text('name', null, ['placeholder' => 'Name']) !!}
+        </div>
 
-<div class="form-group">
-    {!! Form::label('slug', 'Slug') !!}
-    {!! Form::text('slug', null, ['placeholder' => 'Slug']) !!}
-</div>
+        <div class="form-group">
+            {!! Form::label('slug', 'Slug') !!}
+            {!! Form::text('slug', null, ['placeholder' => 'Slug']) !!}
+        </div>
+    </div>
 
-<div class="form-group">
-    {!! Form::submit($submitButtonText) !!}
-</div>
+    <div class="column">
+        {!! Form::label('calendars', 'Calendars') !!}
+        @if(isset($schedule) && $schedule->calendars->count())
+            <ul>
+                @foreach($schedule->calendars as $calendar)
+                    <li>
+                        {!! Form::hidden('calendars[]', $calendar->id) !!}
+                        {{ $calendar->name }}
+                    </li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
+</section>
+
+<section class="row">
+    <div class="column">
+        {!! Form::submit($submitButtonText) !!}
+    </div>
+</section>
