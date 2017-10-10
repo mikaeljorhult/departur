abb52423dab4410292b49aebfdfb2f3ed2424a9c
Mikael Jorhult <mikael@jorhult.se>
2017-10-10 17:22:52 +0200

    Destroy relationships when calendars are deleted.

--- tests/Feature/Calendar/DestroyCalendarTest.php
+++ tests/Feature/Calendar/DestroyCalendarTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Departur\Calendar;
+use Departur\Schedule;
 use Departur\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -43,6 +44,27 @@ class DestroyCalendarTest extends TestCase
         $response->assertRedirect('/login');
         $this->assertDatabaseHas('calendars', [
             'name' => $calendar->name
+        ]);
+    }
+
+    /**
+     * Relationships to schedules are destroyed with calendar.
+     *
+     * @return void
+     */
+    public function testRelationshipsAreDestroyedWithCalendar()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $calendar = factory(Calendar::class)->create();
+        $schedule = factory(Schedule::class)->create();
+        $calendar->schedules()->attach($schedule);
+
+        $response = $this->delete('/calendars/' . $calendar->id);
+
+        $response->assertRedirect('/calendars');
+        $this->assertDatabaseMissing('calendar_schedule', [
+            'schedule_id' => $schedule->id,
         ]);
     }
 }
