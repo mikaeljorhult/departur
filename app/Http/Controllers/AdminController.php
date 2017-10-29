b60a75ee95850edcbdde0df1eda4355ebb36ab8c
Mikael Jorhult <mikael@jorhult.se>
2017-10-29 17:23:21 +0100

    Limit administration panel to authenticated users.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,16 @@ namespace Departur\Http\Controllers;
 class AdminController extends Controller
 {
     /**
+     * Constructor.
+     *
+     * Limits access to authenticated users.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
