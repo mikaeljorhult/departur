0b4289901bd250d72a9b667a11065ef2124ca88a
Mikael Jorhult <mikael@jorhult.se>
2017-11-06 16:08:59 +0100

    Save state of hide past checkbox.

--- resources/assets/js/modules/app/hide-past.js
+++ resources/assets/js/modules/app/hide-past.js
@@ -2,11 +2,24 @@ const hidePast = document.getElementById('hide-past-toggle');
 
 // If hide past events checkbox is present.
 if (hidePast) {
+    // Set initial state if previously saved.
+    let initialState = JSON.parse(localStorage.getItem('hide-past'));
+    hidePast.checked = initialState;
+    handleHidePast(initialState);
+
     // Attach click handler.
     hidePast.addEventListener('click', function (event) {
-        document.documentElement.classList.toggle('hide-past', this.checked);
+        handleHidePast(this.checked);
 
         // Stop event from propagating.
         event.stopPropagation();
     });
+}
+
+function handleHidePast(checked) {
+    // Class to HTML element to hide past dates.
+    document.documentElement.classList.toggle('hide-past', checked);
+
+    // Store state in local storage.
+    localStorage.setItem('hide-past', JSON.stringify(checked));
 }
